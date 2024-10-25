dd40ca46b3096fcd3b9e801a84d9b92b197a6f9b
Wils0nDevVC <wilsonvc.developer@gmail.com>
2024-10-24 21:31:25 -0500

    swgger documentacion v2

--- src/author/dto/create-author.dto.ts
+++ src/author/dto/create-author.dto.ts
@@ -1,7 +1,24 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateAuthorDto {
+
+  @ApiProperty({
+    description: 'El nombre del autor',
+    example: 'J.K. Rowling', 
+  })
   @IsString()
   @IsNotEmpty()
-  name: string; // El nombre del autor
+  name: string; 
+
+  
+  @ApiProperty({
+    description: 'Array de IDs de libros asociados al autor (ObjectId de MongoDB)',
+    example: ['671af24789cc951a3d44cb2f'], 
+    required: false,
+  })
+  @IsArray()
+  @IsOptional()
+  @IsMongoId({ each: true }) 
+  books?: string[]; 
 }
--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,20 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsInt, IsMongoId, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
 
 export class CreateBookDto {
 
-
+  @ApiProperty({
+    description: 'El título del libro',
+    example: 'El Alquimista', // Ejemplo de título de libro
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'Número de capítulos del libro',
+    example: 11, 
+  })
   @IsInt()
   @Min(0)
   chapters: number;
 
+  @ApiProperty({
+    description: 'Número de páginas del libro',
+    example: 208,
+  })
   @IsInt()
   @Min(0)
   pages: number;
 
+  @ApiProperty({
+    description: 'Array de IDs de autores asociados al libro (ObjectId de MongoDB)',
+    example: ['671af24789cc951a3d44cb2f'], // Ejemplo de ID de autor
+    required: false,
+  })
   @IsArray()
   @IsOptional()
   @IsMongoId({ each: true }) // Validar que cada autor sea un ObjectId
